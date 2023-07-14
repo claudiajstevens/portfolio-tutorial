fab17a0413730a1e66c65f76bdd17b6b24c27245
Claudia Stevens <claudiajstevens@gmail.com>
2023-07-13 18:46:46 -0700

    "Added Banner Component"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import NavBar from './Components/NavBar';
+import Banner from './Components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
